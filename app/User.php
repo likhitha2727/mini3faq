27b9383a77225ed2149b59a98c68bcae985c63e8
sn596@njit.edu <Naralasetty@123>
2018-11-09 19:49:19 -0500

    Task:Added Questions migrate,model,seeder and added tests for user profile,questions

--- app/User.php
+++ app/User.php
@@ -56,6 +56,10 @@ class User extends Authenticatable
     {
         return $this->hasOne('App\Profile');
     }
+    public function questions()
+    {
+        return $this->hasMany('App\Question');
+    }
 
 
 }
