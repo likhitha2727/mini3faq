f09f3d1d8630d81ecddf5a9e07e558d10104ee2d
sn596@njit.edu <Naralasetty@123>
2018-12-12 17:25:50 -0500

    Task: Laravel dusk install and Homepage tested

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2018-11-21 22:37:17.
+    * Generated on 2018-12-12 18:00:12.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
