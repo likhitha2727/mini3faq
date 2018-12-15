de9bd9e3515064c8f33098c7c2c9b2de347606c7
sn596@njit.edu <Naralasetty@123>
2018-12-15 01:40:36 -0500

    Task: Splitting Question Tests(create question test)

---
+++ tests/Browser/CreateQuestionTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Browser;
+
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class CreateQuestionTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('http://localhost:8000/home')
+                ->value('#email', 's1234567@njit.com')
+                ->value('#password', 'likhitha')
+                ->press('button[type="submit"]')
+                ->assertSee('Questions')
+                ->clickLink('Create question')
+                ->assertSee('Create Question')
+                ->value('#body', 'whatdusk')
+                ->press('#submit')
+                ->click('#navbarDropdown')
+                ->clickLink('Logout')
+                ->assertSee('Laravel');
+        });
+    }
+}
